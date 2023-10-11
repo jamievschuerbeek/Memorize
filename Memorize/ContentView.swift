ee51bfc6d90b8e8530b2a1b206487b1bee70af14
JamieVS <jamie.vanschuerbeek@student.hogent.be>
2023-10-11 12:02:27 +0200

    added scrollview

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -11,39 +11,49 @@ struct ContentView: View {
     let emojis: [String] = ["👻","🕷️", "🎃", "😈"]
     
     var body: some View {
+        VStack{
+            ScrollView{
+                cards
+            }
+        }
+        .padding()
+    }
+    
+    var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
             
             ForEach(emojis.indices, id: \.self){ index in
                 CardView(content: emojis[index])
+                    .aspectRatio(contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
     }
 }
-    
-    struct CardView : View {
-        let content : String
-        @State var isFaceUp: Bool = true
-        var body: some View{
-            ZStack{
-                let base = RoundedRectangle(cornerRadius: 12)
-                Group{
-                    base
-                        .foregroundColor(.white)
-                    base.strokeBorder(lineWidth: 2)
-                    Text(content)
-                        .font(.largeTitle)
-                }
-                .opacity(isFaceUp ? 1 : 0)
-                base.fill().opacity(isFaceUp ? 0 : 1)
-            }.onTapGesture {
-                isFaceUp.toggle()
+
+
+struct CardView : View {
+    let content : String
+    @State var isFaceUp: Bool = true
+    var body: some View{
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group{
+                base
+                    .foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content)
+                    .font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }.onTapGesture {
+            isFaceUp.toggle()
         }
     }
-    
-    #Preview {
-        ContentView()
-    }
+}
+
+#Preview {
+    ContentView()
+}
 
