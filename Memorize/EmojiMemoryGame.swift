a90495e50b25b94fd6205af58385a98326e55b75
Jamie Van Schuerbeek <jamie.vanschuerbeek@student.hogent.be>
2023-10-21 19:08:25 +0200

    changed view logic to use domain

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,13 +7,30 @@
 
 import SwiftUI
 
-class EmojiMemoryGame{
-    private var model = MemoryGame<String>(numberOfPairsOfCards: 4) { index in
-        return ["🍏", "🍎", "🥝", "🍆", "🍕", "🍟", "🥦", "🍞" ][index]
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["🍏", "🍎", "🥝", "🍆", "🍕", "🍟", "🥦", "🍞" ]
+    
+    private static func createMemoryGame() -> MemoryGame<String>{
+        return MemoryGame<String>(numberOfPairsOfCards: 8) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            }
+            else {
+                return "⁉"
+            }
+        }
     }
+    
+    @Published private var model = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card>{
         return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
     }
     
     func choose(_ card: MemoryGame<String>.Card){
